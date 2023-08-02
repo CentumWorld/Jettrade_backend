669b69ee28040c989d449f588bc63b6a0caf856d
prashantpkc <prashantkumarpkc@gmail.com>
2023-08-02 16:15:58 +0530

    apiurl

--- routes/fetch-user-details.js
+++ routes/fetch-user-details.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const adminController = require('../controllers/adminController');
 const checkMiddleware = require('../middleware/checkAuth');
 
-router.get('/fetch-user-details', adminController.fetchUserDetails);
+router.get('/fetch-user-details',checkMiddleware.checkAuth, adminController.fetchUserDetails);
 
 
 module.exports = router;
