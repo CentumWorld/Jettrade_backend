504e145281982d4972ab983876c3723acd782520
aniimessh <animeshgupta2.002@gmail.com>
2023-08-19 11:23:42 +0530

    / missing in route

--- routes/fetch_business_Developer_Credit_wallet_Transaction_Details.js
+++ routes/fetch_business_Developer_Credit_wallet_Transaction_Details.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin, authorizeAdmin} = require('../middleware/checkAuth');
 
-router.get('fetch-business-developer-credit-wallet-transaction-details-membe',authenticateAdmin,authorizeAdmin, adminController.fetchBusinessDeveloperCreditwalletTransactionDetails);
+router.get('/fetch-business-developer-credit-wallet-transaction-details-membe',authenticateAdmin,authorizeAdmin, adminController.fetchBusinessDeveloperCreditwalletTransactionDetails);
 
 
 module.exports = router;
