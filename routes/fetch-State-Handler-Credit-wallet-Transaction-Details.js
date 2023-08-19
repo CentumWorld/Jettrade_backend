66d2fd3b5626d27934ca520715d2faef0caea53e
prashantpkc <prashantkumarpkc@gmail.com>
2023-08-19 11:26:36 +0530

    route correction

--- routes/fetch-State-Handler-Credit-wallet-Transaction-Details.js
+++ routes/fetch-State-Handler-Credit-wallet-Transaction-Details.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin, authorizeAdmin} = require('../middleware/checkAuth');
 
-router.get('/fetch-user-refferal-payout-approve-withdrawal',authenticateAdmin,authorizeAdmin, adminController.fetchStateHandlerCreditwalletTransactionDetails);
+router.get('/fetch-state-handler-wallet-transaction-details',authenticateAdmin,authorizeAdmin, adminController.fetchStateHandlerCreditwalletTransactionDetails);
 
 
 module.exports = router;
