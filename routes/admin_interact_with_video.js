0c6846643a3fcca92720165afb27f1a140f36592
prashantpkc <prashantkumarpkc@gmail.com>
2023-08-21 16:27:53 +0530

    interact video middlware name  modified

--- routes/admin_interact_with_video.js
+++ routes/admin_interact_with_video.js
@@ -3,9 +3,9 @@ const router = express.Router();
 require('dotenv').config();
 
 const adminController = require('../controllers/adminController');
-const {authenticateAdmin, authorizeAdmin} = require('../middleware/checkAuth');
+const {authenticateAdmin, authorizeAdmin, authorizeRole} = require('../middleware/checkAuth');
 
-router.post('/admin-interact-with-video',authenticateAdmin,authorizeAdmin,adminController.interactWithVideoForAdmin);
+router.post('/admin-interact-with-video',authenticateAdmin,authorizeRole(["admin", "subAdmin" , "state"]),adminController.interactWithVideoForAdmin);
 
 
 module.exports = router;
