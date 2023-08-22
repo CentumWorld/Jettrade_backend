085f660c6f7ef5539bb95628db2841bdf9fabfd9
prashantpkc <prashantkumarpkc@gmail.com>
2023-08-22 17:43:42 +0530

    get to post

--- routes/state/fetch-business-developers-in-state.js
+++ routes/state/fetch-business-developers-in-state.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router()
 const {  getBusinessDevelopersInState } = require("../../controllers/stateController");
 const { authenticateAdmin, authorizeRole } = require("../../middleware/checkAuth");
-router.get('/fetch-all-business-developers-in-state',authenticateAdmin,authorizeRole(["admin", "state"]),getBusinessDevelopersInState);
+router.post('/fetch-all-business-developers-in-state',authenticateAdmin,authorizeRole(["admin", "state"]),getBusinessDevelopersInState);
 module.exports = router;
