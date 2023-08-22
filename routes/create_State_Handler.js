a0133cf3a4041af72841013a16d6eff2cfc40ae7
prashantpkc <prashantkumarpkc@gmail.com>
2023-08-22 14:43:15 +0530

    route modified

--- routes/create_State_Handler.js
+++ routes/create_State_Handler.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/aws'); 
 const adminController = require('../controllers/adminController');
-const { authenticateAdmin, authorizeAdmin, authorizeRole } = require('../middleware/checkAuth');
+const { authenticateAdmin, authorizeRole } = require('../middleware/checkAuth');
 
 router.post(
   '/create_State_Handler',
   upload.fields([{ name: 'adharCard' }, { name: 'panCard'}]),
   authenticateAdmin,
-  authorizeRole(["admin", "state"]),
+  authorizeRole(["admin", "franchise"]),
   adminController.createStateHandler
 );
 
