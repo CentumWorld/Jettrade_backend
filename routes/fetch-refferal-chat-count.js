880d8732b7ed2e9ad806e58f4bd181bf0df994ce
centum-world <centumworldminingrig@gmail.com>
2023-08-28 15:51:40 +0530

    fetchRefferalChatCount aithorizrole

--- routes/fetch-refferal-chat-count.js
+++ routes/fetch-refferal-chat-count.js
@@ -3,7 +3,7 @@ const router = express.Router();
 require('dotenv').config();
 
 const adminController = require('../controllers/adminController');
-const {authenticateAdmin, authorizeAdmin} = require('../middleware/checkAuth');
+const {authenticateAdmin, authorizeRole} = require('../middleware/checkAuth');
 
 router.get('/fetch-refferal-chat-count',authenticateAdmin,authorizeRole(["admin"]), adminController.fetchRefferalChatCount);
 
