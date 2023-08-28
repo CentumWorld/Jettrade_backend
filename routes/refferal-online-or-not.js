acf82c648952b327a2b3d394f1ca3d3bbb59c936
centum-world <centumworldminingrig@gmail.com>
2023-08-28 15:57:06 +0530

    refferalOnlineOrNot(authorizeRole)

--- routes/refferal-online-or-not.js
+++ routes/refferal-online-or-not.js
@@ -3,8 +3,8 @@ const router = express.Router();
 require('dotenv').config();
 
 const adminController = require('../controllers/adminController');
-const {authenticateAdmin, authorizeAdmin} = require('../middleware/checkAuth');
-router.post('/refferal-online-or-not',authenticateAdmin,authorizeAdmin, adminController.refferalOnlineOrNot);
+const {authenticateAdmin, authorizeRole} = require('../middleware/checkAuth');
+router.post('/refferal-online-or-not',authenticateAdmin,authorizeRole(["admin"]), adminController.refferalOnlineOrNot);
 
 
 module.exports = router;
