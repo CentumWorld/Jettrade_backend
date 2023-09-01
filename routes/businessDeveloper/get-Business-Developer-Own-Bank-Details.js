57770917bf81ff4f5001385a9cf5100de5aaa79b
prashantpkc <prashantkumarpkc@gmail.com>
2023-09-01 18:59:44 +0530

    dd

--- routes/businessDeveloper/get-Business-Developer-Own-Bank-Details.js
+++ routes/businessDeveloper/get-Business-Developer-Own-Bank-Details.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router()
 const { getBusinessDeveloperOwnBankDetails } = require("../../controllers/businessDevController");
 const { authenticateAdmin, authorizeRole } = require("../../middleware/checkAuth");
-router.post('/get-business-developer-own-bank-details',authenticateAdmin,authorizeRole([ "businessDeveloper"]),getBusinessDeveloperOwnBankDetails);
+router.post('/get-business-developer-own-bank-details',authenticateAdmin,authorizeRole([ "businessDeveloper", "admin"]),getBusinessDeveloperOwnBankDetails);
 module.exports = router;
--- routes/businessDeveloper/get-Business-Developer-Own-Upi.js
+++ routes/businessDeveloper/get-Business-Developer-Own-Upi.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router()
 const { getBusinessDeveloperOwnUpi } = require("../../controllers/businessDevController");
 const { authenticateAdmin, authorizeRole } = require("../../middleware/checkAuth");
-router.post('/get-business-developer-own-upi',authenticateAdmin,authorizeRole([ "businessDeveloper"]),getBusinessDeveloperOwnUpi);
+router.post('/get-business-developer-own-upi',authenticateAdmin,authorizeRole([ "businessDeveloper", "admin"]),getBusinessDeveloperOwnUpi);
 module.exports = router;
