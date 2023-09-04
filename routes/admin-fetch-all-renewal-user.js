1b3a659ff926656a077c044fd264f454dde340dc
prashantpkc <prashantkumarpkc@gmail.com>
2023-09-04 15:04:30 +0530

    middleware modified

--- routes/admin-fetch-all-renewal-user.js
+++ routes/admin-fetch-all-renewal-user.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin, authorizeAdmin, authorizeRole} = require('../middleware/checkAuth');
 
-router.post('/admin-fetch-all-renewal-user',authenticateAdmin,authorizeRole(["admin"]),adminController.adminFetchAllRenewalUser);
+router.post('/admin-fetch-all-renewal-user',authenticateAdmin,authorizeRole(["admin", "subAdmin"]),adminController.adminFetchAllRenewalUser);
 
 
 module.exports = router;
--- routes/fetch-all-new-paid-user.js
+++ routes/fetch-all-new-paid-user.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin, authorizeRole} = require('../middleware/checkAuth');
 
-router.post('/fetch-all-new-paid-user',authenticateAdmin,authorizeRole(["admin"]),adminController.fetchAllNewPaidUser);
+router.post('/fetch-all-new-paid-user',authenticateAdmin,authorizeRole(["admin", "subAdmin"]),adminController.fetchAllNewPaidUser);
 
 
 module.exports = router;
