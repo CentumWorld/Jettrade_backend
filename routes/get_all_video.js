6ce2400a0646c557a55b60189fbb9f5303cb5eee
prashantpkc <prashantkumarpkc@gmail.com>
2023-09-06 17:20:12 +0530

    ff

--- routes/get_all_video.js
+++ routes/get_all_video.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin,authorizeRole} = require('../middleware/checkAuth');
 
-router.get('/fetch-all-videos',authenticateAdmin, authorizeRole(['admin', 'subAdmin', 'state', "businessDeveloper"]),adminController.getVideos);
+router.get('/fetch-all-videos',authenticateAdmin, authorizeRole(['admin', 'subAdmin', 'state', "businessDeveloper", "videoCreator"]),adminController.getVideos);
 
 
 module.exports = router;
