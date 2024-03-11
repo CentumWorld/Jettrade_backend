6b87e7533af26e4935c0fe616a25720e8849accc
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-03-11 18:25:26 +0530

    pass updated successfully in trader

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const StateHandler = require("../model/stateHandlerSchema");
 const FrachiseHandler = require("../model/frenchiseSchema");
 const MemberHandler = require("../model/memberSchema");
+const TraderHandler =  require("../model/userSchema")
 const { isValidPassword } = require("../validation/validation");
 
 exports.allPasswordChange = async (req, res) => {
@@ -102,6 +103,51 @@ exports.allPasswordChange = async (req, res) => {
 
       const existdetails = await MemberHandler.findOne({ memberid: id });
       console.log('After finding member:', existdetails);
+      if (!existdetails) {
+        return res.status(404).json({
+          message: "Member not found",
+        });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(
+        oldpassword,
+        existdetails.password
+      );
+
+      if (!isPasswordMatch) {
+        return res.status(404).json({
+          message: "Old password does not match",
+        });
+      }
+
+      existdetails.password = newpassword;
+      await existdetails.save();
+
+      res.status(200).json({
+        message: "Password updated successfully",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server error",
+      });
+    }
+  } else if (usertype === "TRADER") {
+    try {
+      if (!oldpassword || !newpassword) {
+        return res.status(422).json({
+          message: "Please fill in your password",
+        });
+      }
+
+      if (!isValidPassword(newpassword)) {
+        return res.status(400).json({
+          message:
+            "New password must have at least one digit, one lowercase letter, one uppercase letter, and be 8 to 15 characters long.",
+        });
+      }
+
+      const existdetails = await TraderHandler.findOne({ userid: id });
       if (!existdetails) {
         return res.status(404).json({
           message: "Member not found",
