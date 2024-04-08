6c27ef5f2ea72bf55a537c3b301ac11a2c34ef69
centum-world <centumworldminingrig@gmail.com>
2024-04-08 11:35:51 +0530

    video portal authorization

--- routes/admin-fetch-user-one-video-like.js
+++ routes/admin-fetch-user-one-video-like.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const adminController = require('../controllers/adminController');
 const {authenticateAdmin,authorizeRole} = require('../middleware/checkAuth');
-router.post('/admin-fetch-user-one-video-like',authenticateAdmin,authorizeRole(["admin","subAdmin", "state", "franchise", "businessDeveloper", "user", "member"]), adminController.AdminfetchUserOneVideoLike);
+router.post('/admin-fetch-user-one-video-like',authenticateAdmin,authorizeRole(["admin","subAdmin", "state", "franchise", "businessDeveloper", "user", "member","videoCreator"]), adminController.AdminfetchUserOneVideoLike);
 
 
 module.exports = router;
