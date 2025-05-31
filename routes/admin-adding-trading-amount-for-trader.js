45bff9d2d688119647eaea04a7bfe95773dab556
Badal-Mandal <mabadal96@gmail.com>
2025-05-31 18:19:03 +0530

    subadmin authorization pass to trading value and history API

--- routes/admin-adding-trading-amount-for-trader.js
+++ routes/admin-adding-trading-amount-for-trader.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const adminController = require('../controllers/adminController');
-const {authenticateAdmin,  authorizeRole} = require('../middleware/checkAuth');
-router.put('/admin-adding-trading-amount-for-trader',authenticateAdmin,authorizeRole(["admin"]), adminController.adminAddingTradingAmountForTrader);
-
+const adminController = require("../controllers/adminController");
+const { authenticateAdmin, authorizeRole } = require("../middleware/checkAuth");
+router.put(
+  "/admin-adding-trading-amount-for-trader",
+  authenticateAdmin,
+  authorizeRole(["admin", "subAdmin"]),
+  adminController.adminAddingTradingAmountForTrader
+);
 
 module.exports = router;
--- routes/admin-fetch-all-renewal-user.js
+++ routes/admin-fetch-all-renewal-user.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
-const adminController = require('../controllers/adminController');
-const {authenticateAdmin, authorizeAdmin, authorizeRole} = require('../middleware/checkAuth');
+const adminController = require("../controllers/adminController");
+const {
+  authenticateAdmin,
+  authorizeAdmin,
+  authorizeRole,
+} = require("../middleware/checkAuth");
 
-router.post('/admin-fetch-all-renewal-user',authenticateAdmin,authorizeRole(["admin", "subAdmin"]),adminController.adminFetchAllRenewalUser);
-
+router.post(
+  "/admin-fetch-all-renewal-user",
+  authenticateAdmin,
+  authorizeRole(["admin", "subAdmin"]),
+  adminController.adminFetchAllRenewalUser
+);
 
 module.exports = router;
